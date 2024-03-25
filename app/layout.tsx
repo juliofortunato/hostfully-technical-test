85f96b98af2873c676bbe069da6f33e35bad9876
Júlio Fortunato <juliofortunato58@gmail.com>
2024-03-25 18:23:08 -0300

    💎 style: fix page height

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,10 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          inter.variable
-        )}
+        className={cn("bg-background font-sans antialiased", inter.variable)}
       >
         <BookingsContextProvider>
           <header className="border-b">
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import BookingList from "@/_components/booking-list";
 
 export default function Home() {
   return (
-    <div className="min-h-screen p-4 xl:p-6">
+    <div className="p-4 xl:p-6">
       <div className="mt-6 grid gap-4">
         <h1 className="text-2xl font-semibold tracking-tight">
           Manage your bookings
